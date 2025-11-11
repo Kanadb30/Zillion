adeda91ab8275f0b17d505f2586a2021910554b7
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-11-11 18:08:12 +0530

    API's created for stores data on admin area

---
+++ app/api/admin/dashboard/route.js
@@ -0,0 +1,51 @@
+import prisma from "@/lib/prisma";
+import authAdmin from "@/middlewares/authAdmin";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request) {
+
+    try {
+        const { userId } = getAuth(request)
+        const isAdmin = await authAdmin(userId)
+        if (!isAdmin) {
+            return NextResponse.json({ error: 'not authorized' },
+                { status: 401 });
+        }
+        // Get total orders
+        const orders = await prisma.order.count()
+        // Get total stores on app
+        const stores = await prisma.store.count()
+        // get all orders include only createdAt and total & calculate
+        //total revenue
+        const allorders = await risma.order.findMany({
+            select: {
+                createdAt: true,
+                total: true,
+            }
+        })
+        let totalRevenue = 0
+        allorders.forEach(order => {
+            totalRevenue += order.total
+        })
+        const revenue = totalRevenue.toFixed(2)
+        // total products on app
+        const products = await prisma.product.count()
+        const dashboardData = {
+            orders,
+            stores,
+            products,
+            revenue,
+            allorders
+        }
+        return NextResponse.json({})
+    }
+    catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message }, {
+            status: 400
+        })
+    }
+
+}
